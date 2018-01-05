a931a447fb4f47a042e54ece92bf10e8bc534ff9
Lucas Azzola <derflatulator@gmail.com>
2018-01-05 11:46:19 +1100

    .

---
+++ test.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const getPackages = require('.');
+
+const LERNA_DIR = path.join(__dirname, 'fixture/lerna');
+const YARN_DIR = path.join(__dirname, 'fixture/yarn');
+
+expect.addSnapshotSerializer({
+  test: value =>
+    typeof value === 'string' &&
+    (value.indexOf('\\') > -1 || value.indexOf(process.cwd()) > -1),
+  print: (value, serializer) =>
+    serializer(value.replace(process.cwd(), '<cwd>').replace(/\\/g, '/')),
+});
+
+describe('getPackages()', () => {
+  test('lerna matches snapshot', () => {
+    expect(getPackages(LERNA_DIR)).toMatchSnapshot();
+  });
+
+  test('yarn matches snapshot', () => {
+    expect(getPackages(YARN_DIR)).toMatchSnapshot();
+  });
+
+  test('returns empty array when no packages', () => {
+    expect(getPackages(__dirname)).toEqual([]);
+  });
+});
